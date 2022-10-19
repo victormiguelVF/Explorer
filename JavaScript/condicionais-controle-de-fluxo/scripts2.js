0320070c8bc979b00d81d8c94f26475e7d9ed8bf
Victor Miguel Vilela Fernandes <113549997+victormiguelVF@users.noreply.github.com>
2022-10-19 15:25:52 -0300

    Condicionais e controle de fluxo JS

---
+++ JavaScript/condicionais-controle-de-fluxo/scripts2.js
@@ -0,0 +1,26 @@
+// switch
+
+function calculate(number1, operator, number2) {
+    let result = 0;
+
+    switch (operator) {
+        case '+':
+            result = number1 + number2
+            break
+        case '-':
+            result = number1 - number2
+            break
+        case '*':
+            result = number1 * number2
+            break
+        case '/':
+            result = number1 / number2
+            break
+        default:
+            console.log('Não implementado')
+            break
+    }
+    return result
+}
+
+console.log(calculate(4, '1', 9))
